a0ac7af786cb114449934a8041dbfa7b18a2d3d5
Thato <t.chitja369@gmail.com>
2025-11-17 15:16:58 +0200

    Add HomeServices, Contact and About Components aalong with their styling

--- HomeServices/src/app/app.routes.ts
+++ HomeServices/src/app/app.routes.ts
@@ -2,4 +2,8 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {path: '', loadComponent: () => import('./home/home.component').then(m => m.HomeComponent)},
+    {path: 'about', loadComponent: () => import('./about/about.component').then(m => m.AboutComponent)},
+    {path: 'contact', loadComponent: () => import('./contact/contact.component').then(m => m.ContactComponent)},
+    {path: 'services', loadComponent: () => import('./home-services/home-services.component').then(m => m.HomeServicesComponent)},
+
 ];
---
+++ HomeServices/src/app/home-services/home-services.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-services',
+  imports: [],
+  templateUrl: './home-services.component.html',
+  styleUrl: './home-services.component.css'
+})
+export class HomeServicesComponent {
+
+}
